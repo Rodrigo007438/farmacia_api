41c789093f4cbd7fa2d9b867c16cbab0099df10d
Rodrigo <joserodrigoguimaraes160401@gmail.com>
2025-11-18 11:37:32 -0300

    Update server.js

--- server.js
+++ server.js
@@ -4,32 +4,20 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 
 const authRoutes = require('./routes/authRoutes');
-
 const remedio_routes = require('./routes/remedioRoutes');
 const pedido_routes = require('./routes/pedidoRoutes');
-
-
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-//const corsOptions = {
-  //origin: 'https://farmacia-react-v2.vercel.app'
-//};
 
-
-
-app.use(cors());
-
-
+app.use(cors()); 
 
 app.use(express.json());
 
-
 mongoose.connect(process.env.MONGO_URI)
-    .then(() => 
-        console.log('CONECTANDO AO BANCO DE DADOS'))
-        .catch((err) =>console.error('Erro ao conectar:', err));
+    .then(() => console.log('CONECTANDO AO BANCO DE DADOS'))
+    .catch((err) => console.error('Erro ao conectar:', err));
 
 app.use('/api/remedios', remedio_routes);
 app.use('/api/pedidos', pedido_routes);
@@ -38,4 +26,3 @@ app.use('/api/auth', authRoutes);
 app.listen(PORT, () =>{
     console.log(`Servidor rodando na porta ${PORT}`);
 });
-
