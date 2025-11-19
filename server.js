e1dc010256354f34b4b5c906e669aa1b57165d43
Rodrigo <joserodrigoguimaraes160401@gmail.com>
2025-11-19 08:30:32 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const express = require('express');
-const cors = require('cors'); // Trazemos ele de volta
+const cors = require('cors'); 
 const mongoose = require('mongoose');
 
 const authRoutes = require('./routes/authRoutes');
@@ -15,31 +15,31 @@ app.get('/teste', (req, res) => {
 });
 
 
-// Configuração do CORS
+
 app.use(cors({
     origin: [
-        'https://farmacia-react-v2.vercel.app', // Seu site na Vercel (SEM BARRA NO FINAL)
-        'https://farmacia-react-v2.vercel.app/', // Garantia caso venha com barra (opcional, mas seguro)
-        'http://localhost:5173', // Vite (Local)
-        'http://localhost:3000'  // Create React App (Local)
+        'https://farmacia-react-v2.vercel.app',
+        'https://farmacia-react-v2.vercel.app/', 
+        'http://localhost:5173', 
+        'http://localhost:3000'  
     ],
-    methods: ['GET', 'POST', 'DELETE', 'PUT', 'PATCH'], // "UPDATE" não existe, removi.
-    allowedHeaders: ['Content-Type', 'Authorization'] // Cabeçalhos comuns
+    methods: ['GET', 'POST', 'DELETE', 'PUT', 'PATCH'], 
+    allowedHeaders: ['Content-Type', 'Authorization'] 
 }));
 
 app.use(express.json());
 
-// Conexão com o Banco
+
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('CONECTANDO AO BANCO DE DADOS'))
     .catch((err) => console.error('Erro ao conectar:', err));
 
-// Rotas
+
 app.use('/api/remedios', remedio_routes);
 app.use('/api/pedidos', pedido_routes);
 app.use('/api/auth', authRoutes);
 
-// Ligar o servidor
+
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
 });
