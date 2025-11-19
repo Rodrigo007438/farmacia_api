05e3488cde312b01c766bdc92a535deddc880355
Rodrigo <joserodrigoguimaraes160401@gmail.com>
2025-11-19 07:25:13 -0300

    Update remedioRoutes.js

--- routes/remedioRoutes.js
+++ routes/remedioRoutes.js
@@ -5,8 +5,8 @@ const router = express.Router();
 const remedio_controller = require('../controllers/remedioController');
 
 router.get('/', remedio_controller.get_remedios);
-//router.post('/', remedio_controller.create_remedios);
-//router.put('/:id', remedio_controller.update_remedios);
-//router.delete('/:id', remedio_controller.delete_remedios);
+router.post('/', remedio_controller.create_remedios);
+router.put('/:id', remedio_controller.update_remedios);
+router.delete('/:id', remedio_controller.delete_remedios);
 
 module.exports = router;
